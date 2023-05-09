59edda6bed83ec15813092dc61bfa0175632c930
Suitret <suitret01@gmail.com>
2023-05-09 02:37:39 -0700

    3. When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(97, 123):
     char = chr(i)
-    print(f"{char}", end='')
+    print("{}".format(char), end='')
---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for i in range(100):
+	if i < 99 :
+        print(f"{i:.2d}", end=", ")
+	else:
+        print(f"{i:.2d}")
