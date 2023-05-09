48959f9bcd65675ffb1e027e4882b146363860fe
Suitret <suitret01@gmail.com>
2023-05-09 02:48:01 -0700

    5. 00...99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
-	if i < 99 :
-        print(f"{i:.2d}", end=", ")
-	else:
-        print(f"{i:.2d}")
+    if i < 99:
+        print("{:.2d}".format(i), end=", ")
+    else:
+        print("{:.2d}".format(i))
