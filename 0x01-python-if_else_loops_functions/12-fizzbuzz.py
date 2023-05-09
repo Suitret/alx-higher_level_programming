3c89c43f1f182413a6596b8d9497fa363034f71b
Suitret <suitret01@gmail.com>
2023-05-09 07:10:07 -0700

    12. Fizz Buzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    i = 1
+    print(i, end='')
+    for i in range(2, 101):
+        j = i % 3
+        k = i % 5
+        print(" ", end='')
+        if j == 0:
+            print("Fizz", end='')
+        if k == 0:
+            print("Buzz", end='')
+        if j and k:
+            print(i, end='')
+    print("")
