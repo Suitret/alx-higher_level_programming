49350e19f262affe22d068322d778a898a5da31a
Suitret <suitret01@gmail.com>
2023-05-09 04:25:37 -0700

    8. To uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python3
 def uppercase(string):
-    lenght = len(string)
-    j = 0
     for i in string:
-        j += 1
-        delim = ''
-        if j == lenght:
-            delim = '\n'
         asc = ord(i)
-        if asc >= 97 and asc <= 122:
-            asc = chr(asc - 32)
-            print("{}".format(asc), end=delim)
-        else:
-            asc = chr(asc)
-            print("{}".format(asc), end=delim)
+        asc = chr(asc-32) if asc >= 97 and asc <= 122 else chr(asc)
+        print("{}".format(asc), end='')
+    print("")
