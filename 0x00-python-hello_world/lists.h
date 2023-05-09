5b76e8b8d40fc1ef64094f153cbb14e4a45d2daf
Suitret <suitret01@gmail.com>
2023-05-09 02:33:53 -0700

    10. Linked list cycle

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -17,11 +17,26 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/**
+ * struct cplist - singly linked list
+ * @adr: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ *
+ */
+typedef struct cplist
+{
+	listint_t *adr;
+	struct cplist *next;
+} cplist;
+
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
 int search(listint_t *list, listint_t *node);
-
+cplist *add_nodecp(cplist **head, listint_t *adr);
+void free_cplist(cplist *head);
 #endif /* LISTS_H */
