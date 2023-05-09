6baf1e492d8288705da6232e1f8a2e69ed5041d6
Suitret <suitret01@gmail.com>
2023-05-09 02:26:03 -0700

    10. Linked list cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -3,41 +3,20 @@
 #include "lists.h"
 
 /**
- * add_nodecp - adds a new node
- * @head: pointer to a pointer of the start of the list
- * @adr: adress to be included in node
- * Return: address of the new element or NULL if it fails
+ * search - searches a node in list
+ * @list: given list
+ * @node: given node
+ * Return: 0 or 1
  */
-cplist *add_nodecp(cplist **head, listint_t *adr)
+int search(listint_t *list, listint_t *node)
 {
-	cplist *new;
-
-	new = malloc(sizeof(cplist));
-	if (new == NULL)
-		return (NULL);
-
-	new->adr = adr;
-	new->next = *head;
-	*head = new;
-
-	return (new);
-}
-
-/**
- * free_listcp - frees a listcp list
- * @head: pointer to list to be freed
- * Return: void
- */
-void free_listcp(cplist *head)
-{
-	cplist *current;
-
-	while (head != NULL)
+	while (list)
 	{
-		current = head;
-		head = head->next;
-		free(current);
+		if (list == node)
+			return (1);
+		list = list->next;
 	}
+	return (0);
 }
 
 /**
@@ -47,42 +26,38 @@ void free_listcp(cplist *head)
  */
 int check_cycle(listint_t *list)
 {
-	cplist *head = NULL, *temp, *cur;
-	listint_t *ptr;
-	int flag = 0;
+	listint_t *head = NULL, *temp, *tmp;
 
 	if (!list)
 		return (0);
+
+	head = list;
+	list = list->next;
+	head->next = NULL;
+
 	while (list)
 	{
-		temp = add_nodecp(&head, list);
-		ptr = list;
+		if (search(head, list))
+			break;
+		temp = list;
 		list = list->next;
-		ptr->next = NULL;
+		temp->next = head;
+		head = temp;
 	}
-	cur = head->next;
-	while (cur)
-	{
-		if (cur->adr == temp->adr)
-		{
-			flag = 1;
-			break;
-		}
-		cur = cur->next;
-	}
-	cur = (flag == 1) ? (head->next) : (head);
-	while (cur)
-	{
-		temp = cur->next;
-		if (temp)
-			temp->adr->next = cur->adr;
-		else
-			break;
-		cur = cur->next;
-	}
-	if (flag)
-		head->next->adr->next = head->adr;
 
-	free_listcp(head);
-	return (flag);
+	temp = head;
+	head = head->next;
+	temp->next = list;
+
+	while (head)
+	{
+		tmp = head;
+		head = head->next;
+		tmp->next = temp;
+		temp = tmp;
+	}
+	if (list)
+		return (1);
+	else
+		return (0);
 }
--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -17,26 +17,11 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/**
- * struct cplist - singly linked list
- * @adr: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- *
- */
-typedef struct cplist
-{
-	listint_t *adr;
-	struct cplist *next;
-} cplist;
-
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
-cplist *add_nodecp(cplist **head, listint_t *adr);
-void free_cplist(cplist *head);
+int search(listint_t *list, listint_t *node);
 
 #endif /* LISTS_H */
