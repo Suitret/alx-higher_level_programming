a0263a181a887c05ddad5c9528e599d5e1e791a1
Suitret <suitret01@gmail.com>
2023-05-11 02:12:04 -0700

    2. How to make a script dynamic!

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 from add_0 import add
 
-a = 1
-b = 2
+def main():
+    a = 1
+    b = 2
 
-print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
 
 if __name__ == "__main__":
     main()
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
 import sys
 
-lenght = len(argv)
+def main():
+    argv = sys.argv[1:]
+    argc = len(argv)
 
-if lenght is 0:
-    print("0 arguments.")
-else:
-    if lenght is 1:
-        print("1 argument:")
+    if argc == 0:
+        print("0 arguments.")
     else:
-        print(lenght, "arguments:")
+        if argc == 1:
+            print("1 argument:")
+        else:
+            print(argc, "arguments:")
 
-    i = 1
-    for arg in argv:
-        print("{:d}: {}".format(i, arg))
-        i += 1
+        i = 1
+        for arg in argv:
+            print("{:d}: {}".format(i, arg))
+            i += 1
 
 if __name__ == "__main__":
     main()
