bb9278e1a640d0c4e66dcc6135076dd8fdcd7f75
Suitret <suitret01@gmail.com>
2023-05-12 00:36:50 -0700

    5. Can you C me now?

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+    lenght = len(my_string)
+    new_string = ""
+    for i in range(lenght):
+        if my_string[i] not in "cC":
+            new_string += my_string[i]
+    return new_string
