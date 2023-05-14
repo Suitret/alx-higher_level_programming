44e588d9cdd19d29c9bb4f2388234c1e9f783201
Suitret <suitret01@gmail.com>
2023-05-14 10:50:29 -0700

    reversed*

--- 0x03-python-data_structures/main.py
+++ 0x03-python-data_structures/main.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
 print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
 
-my_list = [1]
-print_reversed_list_integer(my_list)
-my_list = [1, 2]
-print_reversed_list_integer(my_list)
-my_list = [1, 2, 3]
-print_reversed_list_integer(my_list)
-my_list = [1, 2, 3, 4, 5]
-print_reversed_list_integer(my_list)
-my_list = []
+my_list = [1, 2, 3, 4]
 print_reversed_list_integer(my_list)
