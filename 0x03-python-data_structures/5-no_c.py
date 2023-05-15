07ce361ef081b84ec6d30eac6e8b99d448c6af17
Suitret <suitret01@gmail.com>
2023-05-14 17:36:52 -0700

    correction

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,9 +3,8 @@ def no_c(my_string):
     if not my_string:
         return
 
-    lenght = len(my_string)
     new_string = ""
-    for i in range(lenght):
-        if my_string[i] not in "cC":
-            new_string += my_string[i]
+    for char in my_string:
+        if char not in "cC":
+            new_string += char
     return new_string
