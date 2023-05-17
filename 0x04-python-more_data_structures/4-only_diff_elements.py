e9a2906fa3b49bf1915de1d541c635e85724a2de
Suitret <suitret01@gmail.com>
2023-05-17 10:00:07 -0700

    correction2

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -5,9 +5,9 @@ def only_diff_elements(set_1, set_2):
     if not set_1 and not set_2:
         return []
     elif not set_1:
-        return list(set_1)
-    elif not set_2:
         return list(set_2)
+    elif not set_2:
+        return list(set_1)
 
-    set_3 = set_1.symmetric_difference(set_2)
+    set_3 = set_1 ^ set_2
     return set_3
