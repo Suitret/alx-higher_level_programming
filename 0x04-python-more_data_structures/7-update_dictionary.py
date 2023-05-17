f0bed0b1e675fb8a895ed131df77b90aa26fb598
Suitret <suitret01@gmail.com>
2023-05-16 22:41:27 -0700

    7. Update dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+
+    if not a_dictionary:
+        a_dictionary = set((key, value))
+    else:
+        a_dictionary[key] = value
+
+    return a_dictionary
