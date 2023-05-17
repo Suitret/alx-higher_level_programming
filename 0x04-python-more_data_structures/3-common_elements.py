b8950c0f62ed3e7e413b0f3bf1a7a109ca7ac4f0
Suitret <suitret01@gmail.com>
2023-05-17 09:50:19 -0700

    correction

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -3,7 +3,7 @@
 def common_elements(set_1, set_2):
 
     if not set_1 or not set_2:
-        return None
+        return []
 
-    set_3 = set_1.intersection(set_2)
+    set_3 = set_1 & set_2
     return set_3
--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -3,7 +3,7 @@
 def only_diff_elements(set_1, set_2):
 
     if not set_1 or not set_2:
-        return None
+        return []
 
     set_3 = set_1.symmetric_difference(set_2)
     return set_3
