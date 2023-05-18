e6988f4b2a7cd22ad8a58abcad051ef5ebe90348
Suitret <suitret01@gmail.com>
2023-05-18 02:35:06 -0700

    correction5

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -4,6 +4,8 @@ def update_dictionary(a_dictionary, key, value):
 
     if not a_dictionary:
         new_dict = dict()
+    else:
+        new_dict = a_dictionary
 
     new_dict[key] = value
     a_dictionary = new_dict
