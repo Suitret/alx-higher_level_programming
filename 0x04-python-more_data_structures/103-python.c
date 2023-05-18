ef94374a136e33842eaec2684101ea5a9e5f6fde
Suitret <suitret01@gmail.com>
2023-05-18 10:01:08 -0700

    cpython

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -2,6 +2,32 @@
 #include <stdio.h>
 
 /**
+ * print_python_bytes - Print some basic info about Python bytes
+ * @p: A pointer to a PyObject list
+ * Return: void
+ */
+void print_python_bytes(PyObject *p)
+{
+	PyBytesObject *bytes = (PyBytesObject *)p;
+	Py_ssize_t size = PyBytes_Size(p);
+	Py_ssize_t i;
+	char *str;
+
+	printf("[.] bytes object info\n");
+	printf("  size: %ld\n", size);
+	printf("  trying string: %s\n", PyBytes_AsString(p));
+
+	printf("  first %ld bytes: ", (size > 10) ? 10 : size);
+	str = (char *)bytes->ob_sval;
+	for (i = 0; i < size && i < 10; i++)
+	{
+		printf("%02x", (unsigned char)str[i]);
+		if (i < size - 1 && i < 9)
+			printf(" ");
+	}
+	printf("\n");
+}
+/**
  * print_python_list - Print some basic info about Python lists
  * @p: A pointer to a PyObject list
  * Return: void
@@ -28,31 +54,4 @@ void print_python_list(PyObject *p)
 		if (strcmp(type, "bytes") == 0)
 			print_python_bytes(item);
 	}
-}
-
-/**
- * print_python_bytes - Print some basic info about Python bytes
- * @p: A pointer to a PyObject list
- * Return: void
- */
-void print_python_bytes(PyObject *p)
-{
-	PyBytesObject *bytes = (PyBytesObject *)p;
-	Py_ssize_t size = PyBytes_Size(p);
-	Py_ssize_t i;
-	char *str;
-
-	printf("[.] bytes object info\n");
-	printf("  size: %ld\n", size);
-	printf("  trying string: %s\n", PyBytes_AsString(p));
-
-	printf("  first %ld bytes: ", (size > 10) ? 10 : size);
-	str = (char *)bytes->ob_sval;
-	for (i = 0; i < size && i < 10; i++)
-	{
-		printf("%02x", (unsigned char)str[i]);
-		if (i < size - 1 && i < 9)
-			printf(" ");
-	}
-	printf("\n");
 }
