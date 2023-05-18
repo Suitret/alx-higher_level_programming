e2d8facaedf2061812fafa165a02326f75d5a47c
Suitret <suitret01@gmail.com>
2023-05-18 04:45:57 -0700

    correction

--- 0x04-python-more_data_structures/main.py
+++ 0x04-python-more_data_structures/main.py
@@ -3,17 +3,29 @@
 """
 roman_to_int = __import__('12-roman_to_int').roman_to_int
 
-roman_number = "X"
+roman_number = "I"
 print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
-roman_number = "VII"
+roman_number = "III"
 print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
-roman_number = "IX"
+roman_number = "XXI"
 print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
-roman_number = "LXXXVII"
+roman_number = "IV"
 print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
-roman_number = "DCCVII"
+roman_number = "CXXIV"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = 1423
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = None
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "LXXXIX"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "XCIX"
 print("{} = {}".format(roman_number, roman_to_int(roman_number)))
