dead6c41ad3008f5dff84b7c72996a5b3bc187ff
Suitret <suitret01@gmail.com>
2023-05-29 02:53:33 -0700

    3. Integers division with debug

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -7,6 +7,6 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         result = None
     finally:
-        print("Inside result:", result)
+        print("Inside result: {}".format(result))
 
     return result
