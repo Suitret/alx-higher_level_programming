66e22a9e90fba95c89e671563700d8d5710f3538
Suitret <suitret01@gmail.com>
2023-05-28 21:36:49 -0700

    0. Safe list printing

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+
+    num = 0
+    for i in range(x):
+        try:
+               print("{}".format(my_list[i]), end="")
+        except:
+               break
+        num += 1
+    print()
+    return num
