ed5b445112a8fd514498ce14bfd56d8793db8435
Suitret <suitret01@gmail.com>
2023-05-30 01:30:58 -0700

    1. Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+class Square:
+    """class Square that defines a square"""
+    def __init__(self, size):
+        """function that initializes attributes
+        Args:
+            self : the object
+            size (int): size of the square
+        Returns:
+            void
+        """
+        self.__size = size
