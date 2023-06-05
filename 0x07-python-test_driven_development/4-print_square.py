0ad3cef0f6c04ad9177adad4b0c8c6602cd3c16f
Suitret <suitret01@gmail.com>
2023-06-05 09:22:50 -0700

    python programs

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Module which contains print_square
+   function
+"""
+
+
+def print_square(size):
+    """function that prints a square
+    Args:
+        size : the size
+    Returns:
+        void
+    """
+    msg = "size must be an integer"
+
+	if not isinstance(size, int):
+        raise TypeError(msg)
+
+    if isinstance(size, float) and size < 0:
+        raise TypeError(msg)
+
+	if size <= 0:
+        raise ValueError("size must be >= 0")
+
+    size = int(size)
+    line = '#' * size
+    for _ in range(size):
+        print(line)
