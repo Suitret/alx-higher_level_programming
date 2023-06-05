e8906e8ffca7017f972ce4dfb4a9a00e576c8594
Suitret <suitret01@gmail.com>
2023-06-05 09:04:26 -0700

    python programs

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module which contains say_my_name
+   function
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """function that says the name
+    Args:
+        first_name : the first name
+        last_name : the last name
+    Returns:
+        void
+    """
+
+    if not isinstance(first_name, str)
+        raise TypeError("first_name must be a string")
+
+    if not isinstance(last_name, str)
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
