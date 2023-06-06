b506659df8896f068d986f4d7d9d07ea434f3c0b
Suitret <suitret01@gmail.com>
2023-06-06 12:33:37 -0700

    30. Low memory cost

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,22 @@
 #!/usr/bin/python3
-class :
+"""Module containing LockedClass"""
+
+
+class LockedClass:
+    """This is the LockedClass"""
+
     __slots__ = ('first_name')
 
     def __setattr__(self, name, value):
+        """function to authorize dynamic
+           attribute
+           Args:
+               self : the object
+               name : the attribute name
+               value : the attribute value
+           Return:
+               void
+        """
+
         if hasattr(self, name):
             super().__setattr__(name, value)
