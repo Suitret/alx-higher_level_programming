41b896bc86b5de4bb07b6b25c7f6360b457b74ae
Suitret <suitret01@gmail.com>
2023-06-12 01:56:45 -0700

    first commit

--- 0x06-python-classes/main.py
+++ 0x06-python-classes/main.py
@@ -1,22 +1,16 @@
 #!/usr/bin/python3
-Node = __import__('100-singly_linked_list').Node
+SinglyLinkedList = __import__('100-singly_linked_list').SinglyLinkedList
 
-n1 = Node(3)
-print(n1.data)
-
-n2 = Node(-5)
-print(n2.data)
-
-n3 = Node(4)
-n3.next_node = n2
-print(n3.next_node.data)
-
-try:
-    n4 = Node("4")
-except Exception as e:
-    print(e)
-
-try:
-    n2.next_node = "Node"
-except Exception as e:
-    print(e)
+sll = SinglyLinkedList()
+sll.sorted_insert(2)
+sll.sorted_insert(5)
+sll.sorted_insert(3)
+sll.sorted_insert(10)
+sll.sorted_insert(1)
+sll.sorted_insert(-4)
+sll.sorted_insert(-3)
+sll.sorted_insert(4)
+sll.sorted_insert(5)
+sll.sorted_insert(12)
+sll.sorted_insert(3)
+print(sll)
