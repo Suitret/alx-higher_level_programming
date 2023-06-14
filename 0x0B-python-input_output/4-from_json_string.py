1e7b371784546ee0d2dec2dc06cbcdbe1217b2b0
Suitret <suitret01@gmail.com>
2023-06-14 02:32:29 -0700

    4. From JSON string to Object

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module containing from_json_string function
+"""
+
+
+def from_json_string(my_str):
+    """function that returns an object (Python data structure)
+       represented by a JSON string
+        Args:
+            my_str : the object
+        Returns:
+            Python object
+    """
+    import json
+    return json.loads(my_obj)
