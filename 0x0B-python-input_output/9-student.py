5de74805f4bf12ce77fbcda3b94e3514d7898281
Suitret <suitret01@gmail.com>
2023-06-14 09:52:40 -0700

    9. Student to JSON

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Module containing read_file function
+"""
+
+
+class Student:
+    """class Student that is not empty"""
+
+    def __init__(self, first_name, last_name, age):
+        """function that initializes attributes
+        Args:
+            self : the object
+            first_name : the first name
+            last_name : the last name
+            age : the age
+        Returns:
+            void
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """function that retrieves a
+           dictionary representation of a Student
+        Args:
+            self : the object
+        Returns:
+            dict
+        """
+        return self.__dict__.items()
