35ef8674badb9b774bce056e1477bbd29f29d498
Suitret <suitret01@gmail.com>
2023-06-14 17:20:48 -0700

    12. Pascal's Triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -16,12 +16,11 @@ def pascal_triangle(n):
         return [[1]]
     else:
         L = [[1], [1, 1]]
-        for j in range(n):
-            temp = []
-            for i in range(n):
-                if i == 0 or i == n-1:
-                    temp.append(1)
-                else:
-                    temp.append(L[n-2][i] + L[n-2][i-1])
+        for j in range(2, n):
+            temp = [1]
+            pr = L[j-1]
+            for i in range(1, j):
+                temp.append(pr[i] + pr[i-1])
+            temp.append(1)
             L.append(temp)
         return L
