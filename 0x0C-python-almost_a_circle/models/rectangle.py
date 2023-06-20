4e2c1256b77663ea94cc4bbe9521abcc1ebc0bb1
Suitret <suitret01@gmail.com>
2023-06-20 08:27:50 -0700

    rectangle class to_dictionary method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -150,3 +150,16 @@ class Rectangle(Base):
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        """Returns the dictionary representation of the rectangle
+        Returns:
+            dict: The dictionary representation of the rectangle
+        """
+        return {
+            'id': self.id,
+            'width': self.__width,
+            'height': self.__height,
+            'x': self.__x,
+            'y': self.__y
+        }
