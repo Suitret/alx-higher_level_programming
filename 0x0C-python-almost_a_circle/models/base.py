d1492650e030da4fb8f396b38341ad78ea4c728c
Suitret <suitret01@gmail.com>
2023-06-20 08:59:53 -0700

    loading

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -44,3 +44,15 @@ class Base:
             json_list = [obj.to_dictionary() for obj in list_objs]
         with open(filename, "w") as file:
             file.write(cls.to_json_string(json_list))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns the list represented by json_string
+        Args:
+            json_string (str): The JSON string representation
+        Returns:
+            list: The list represented by json_string
+        """
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
