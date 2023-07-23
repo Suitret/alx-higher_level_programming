2480c9f7da1c2c08dfc56cf995469b0f54bf9cbf
Suitret <suitret01@gmail.com>
2023-07-23 09:43:01 -0700

    1. Root user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,9 @@
 -- create plus access
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+CREATE USER
+    IF NOT EXISTS 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES
+   ON *.*
+   TO 'user_0d_1'@'localhost'
+   IDENTIFIED BY 'user_0d_1_pwd';
+FLUSH PRIVILEGES;
