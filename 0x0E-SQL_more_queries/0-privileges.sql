bb4cfc85fcd79a759cde7070802d59a6536e7339
Suitret <suitret01@gmail.com>
2023-07-23 09:22:38 -0700

    0. My privileges!

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- list all privileges
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SELECT * FROM mysql.user WHERE User = 'user_0d_1' AND Host = 'localhost';
+SELECT * FROM mysql.user WHERE User = 'user_0d_2' AND Host = 'localhost';
