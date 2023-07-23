117a944d99d6866784ce57238b06d78648267d2f
Suitret <suitret01@gmail.com>
2023-07-23 09:45:28 -0700

    3. Always a name

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,6 +1,4 @@
--- temp
-USE your_database_name;
-
+-- script that creates the table force_name on your MySQL server.
 CREATE TABLE IF NOT EXISTS force_name (
     id INT,
     name VARCHAR(256) NOT NULL
