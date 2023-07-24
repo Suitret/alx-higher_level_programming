2e933c22627659f2a295a4fb8ead53e463655e7e
Suitret <suitret01@gmail.com>
2023-07-23 19:14:07 -0700

    8. Cities of California

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,2 @@
+-- script that lists all the cities of California that can be found in the database hbtn_0d_usa.
+WITH my_id AS (SELECT id FROM states WHERE name = 'California') SELECT id, name FROM cities WHERE cities.state_id = my_id.id GROUP BY name ORDER BY cities.id ASC;
