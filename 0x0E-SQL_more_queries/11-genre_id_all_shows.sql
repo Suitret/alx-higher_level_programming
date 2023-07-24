1f27115d3a8a73a96d70271eac1bc71889d4dc01
Suitret <suitret01@gmail.com>
2023-07-23 20:09:21 -0700

    11. Genre ID for all shows

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,10 +1,7 @@
 -- script that lists all shows contained in the database hbtn_0d_tvshows.
 SELECT
 	tv_shows.title,
-	CASE
-        WHEN tv_show_genres.genre_id IS NOT NULL THEN tv_show_genres.genre_id
-		ELSE NULL
-    END AS 'genre_id'
+    tv_show_genres.genre_id
 	FROM tv_shows
 	JOIN tv_show_genres
 	WHERE tv_shows.id = tv_show_genres.show_id 
