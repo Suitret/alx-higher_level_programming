f944ad927e98a1c731bf89c6e80cbee9115480c2
Suitret <suitret01@gmail.com>
2023-07-23 20:14:02 -0700

    11. Genre ID for all shows

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -3,6 +3,6 @@ SELECT
 	tv_shows.title,
     tv_show_genres.genre_id
 	FROM tv_shows
-	JOIN tv_show_genres
+	LEFT JOIN tv_show_genres
 	WHERE tv_shows.id = tv_show_genres.show_id 
 	ORDER BY tv_shows.title, tv_show_genres.genre_id;
