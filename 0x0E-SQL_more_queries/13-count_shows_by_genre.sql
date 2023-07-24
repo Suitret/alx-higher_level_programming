4edfac6ae5b2f5e2e9cb6d5fd109fa316f517742
Suitret <suitret01@gmail.com>
2023-07-23 21:19:47 -0700

    13. Number of shows by genre

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- script that lists all shows contained in hbtn_0d_tvshows without a genre linked. 
 SELECT
 	A.title AS genre,
-    COUNT(B.genre_id) AS number_of_shows,
+    COUNT(B.genre_id) AS number_of_shows
 	FROM tv_shows AS A
 	LEFT JOIN tv_show_genres AS B
 	ON A.id = B.show_id
