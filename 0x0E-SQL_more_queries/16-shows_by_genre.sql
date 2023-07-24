46ecbe6a67e5c707463c67621e4d54ea8ab12501
Suitret <suitret01@gmail.com>
2023-07-23 22:00:15 -0700

    16. List shows and genres

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -2,6 +2,6 @@
 SELECT
 	A.title, C.name
 	FROM tv_shows AS A
-	JOIN tv_show_genres AS B ON A.id = B.show_id
+	LEFT JOIN tv_show_genres AS B ON A.id = B.show_id
 	LEFT JOIN tv_genres AS C ON B.genre_id = C.id
 	ORDER BY A.title, C.name;
