ab650774c67312d71e5c34c2e5a70efddddf67ae
Suitret <suitret01@gmail.com>
2023-08-14 03:32:45 -0700

    11. Second biggest!

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
+const args = process.argv.slice(2).map(Number);
 
-const inputValue = process.argv[2];
-const intValue = parseInt(inputValue);
-
-if (isNaN(intValue)) {
-  console.log('Not a number');
+if (args.length <= 1) {
+  console.log(0);
 } else {
-  console.log('My number:', intValue);
+  const sortedArgs = args.sort((a, b) => b - a);
+  console.log(sortedArgs[1]);
 }
