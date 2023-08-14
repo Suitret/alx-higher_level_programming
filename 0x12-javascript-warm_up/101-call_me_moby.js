49aef9c335b71c1fb0c1a34a7a3ae37abf2ee6dd
Suitret <suitret01@gmail.com>
2023-08-14 03:43:20 -0700

    15. Call me Moby

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 
-const inputValue = process.argv[2];
-const intValue = parseInt(inputValue);
-
-if (isNaN(intValue)) {
-  console.log('Not a number');
-} else {
-  console.log('My number:', intValue);
+function callMeMoby (x, theFunction) {
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
 }
+
+module.exports.callMeMoby = callMeMoby;
