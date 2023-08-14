dd9bef8f4ebe98aede88a32d9afc86eaa462e7c7
Suitret <suitret01@gmail.com>
2023-08-14 03:51:57 -0700

    17. Increment object

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,20 +1,15 @@
 #!/usr/bin/node
-
 const myObject = {
   type: 'object',
-  value: 12,
-  incr: function () {
-    this.value++;
-  }
+  value: 12
 };
-
 console.log(myObject);
-
+function incr() {
+  this.value += 1;
+}
 myObject.incr();
 console.log(myObject);
-
 myObject.incr();
 console.log(myObject);
-
 myObject.incr();
 console.log(myObject);
