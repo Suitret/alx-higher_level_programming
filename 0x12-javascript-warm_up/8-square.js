956289d503c38aeb57dd0b296230f88cec2f1a22
Suitret <suitret01@gmail.com>
2023-08-14 03:12:48 -0700

    8. Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -6,9 +6,9 @@ const intValue = parseInt(inputValue);
 if (isNaN(intValue)) {
   console.log('Missing size');
 } else {
-  let i = 0, j;
+  let i = 0;
   while (i < intValue) {
     console.log('X'.repeat(intValue));
-	i++;
+    i++;
   }
 }
