bec866c4fa871039610edb05458657a267c57b84
Suitret <suitret01@gmail.com>
2023-08-14 21:19:19 -0700

    2. Rectangle #3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,5 +1,20 @@
 #!/usr/bin/node
 
 class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0) {
+      return;
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    let i = 0;
+    while (i < this.height) {
+      console.log('X'.repeat(this.width));
+      i++;
+    }
+  }
 }
 module.exports = Rectangle;
