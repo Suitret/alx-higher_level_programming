2d4dac548ad2a0f655f0be8a77c0756e611b4f5a
Suitret <suitret01@gmail.com>
2023-08-14 21:32:12 -0700

    6. Square #1

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,5 +1,21 @@
 #!/usr/bin/node
 
-class Rectangle {
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size); // Call the parent class constructor
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    const row = c.repeat(this.width);
+    for (let i = 0; i < this.height; i++) {
+      console.log(row);
+    }
+  }
 }
-module.exports = Rectangle;
+
+module.exports = Square;
