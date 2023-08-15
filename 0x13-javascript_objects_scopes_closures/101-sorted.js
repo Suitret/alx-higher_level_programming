0f1972c7f2445fc86dac41af888e110fe90f34d8
Suitret <suitret01@gmail.com>
2023-08-14 22:11:08 -0700

    11. Factor index

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,5 +1,15 @@
 #!/usr/bin/node
 
-class Rectangle {
+const { dict } = require('./101-data');
+
+const resultDict = {};
+
+for (const userId in dict) {
+  const occurrence = dict[userId];
+  if (resultDict[occurrence] === undefined) {
+    resultDict[occurrence] = [];
+  }
+  resultDict[occurrence].push(userId);
 }
-module.exports = Rectangle;
+
+console.log(resultDict);
