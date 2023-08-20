6479fa2e114ca463b5545f580b6b1ec76b2a3be5
Suitret <suitret01@gmail.com>
2023-08-20 05:35:48 -0700

    1. Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,3 +1,22 @@
 #!/usr/bin/python3
-"""script that """
+"""script that lists all states with a name starting with N (upper N)
+   from the database
+"""
 
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    mydb = MySQLdb.connect(host="localhost",
+                           port=3306,
+                           user=argv[1],
+                           passwd=argv[2],
+                           db=argv[3])
+    cursor = mydb.cursor()
+    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+    cursor.execute(query)
+    query_rows = cursor.fetchall()
+    for row in query_rows:
+        print(row)
+    cursor.close()
+    mydb.close()
--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,3 +1,20 @@
 #!/usr/bin/python3
-"""script that """
+"""script that lists all states from the database hbtn_0e_0_usa"""
 
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    mydb = MySQLdb.connect(host="localhost",
+                           port=3306,
+                           user=argv[1],
+                           passwd=argv[2],
+                           db=argv[3])
+    cursor = mydb.cursor()
+    query = "SELECT * FROM states ORDER BY id ASC"
+    cursor.execute(query)
+    query_rows = cursor.fetchall()
+    for row in query_rows:
+        print(row)
+    cursor.close()
+    mydb.close()
