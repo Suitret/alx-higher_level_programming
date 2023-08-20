206e45e65c5a094f7f7cd5ea51cf483663863705
Suitret <suitret01@gmail.com>
2023-08-20 10:45:41 -0700

    2. Filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
                            passwd=argv[2],
                            db=argv[3])
     cursor = mydb.cursor()
-    query = "SELECT * FROM states WHERE name REGEXP '{}' ORDER BY id ASC;"
+    query = "SELECT * FROM states WHERE name='{}' ORDER BY id ASC;"
     cursor.execute(query.format(argv[4]))
     query_rows = cursor.fetchall()
     for row in query_rows:
