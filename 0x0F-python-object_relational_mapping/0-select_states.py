c88ca7bc92c552e2d47b7c750982c700629179a8
Suitret <suitret01@gmail.com>
2023-08-20 05:19:07 -0700

    0. Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
 """script that lists all states from the database hbtn_0e_0_usa
 """
-
-import MySQLdb as mysql
+import MySQLdb
 import sys
 
-args = sys.argv[1:]
 
-if __name__ == "__main__":
-    mydb = mysql.connect(
-        host="localhost",
-        user=args[0],
-        passwd=args[1],
-        db=args[2],
-        port="3306"
-    )
-    cursor = mydb.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
-    cursor.close()
-    mydb.close()
+args = sys.argv[1:]
+mydb = MySQLdb.connect(
+    host="localhost",
+    user=args[0],
+    passwd=args[1],
+    db=args[2],
+    port="3306"
+)
+cursor = mydb.cursor()
+query = "SELECT * FROM states ORDER BY id ASC"
+cursor.execute(query)
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
+cursor.close()
+mydb.close()
