aba21ce626053460ef28f99515f76f3638683adf
Suitret <suitret01@gmail.com>
2023-08-20 10:51:10 -0700

    3. SQL Injection...

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""script that lists all states from the database hbtn_0e_0_usa"""
+"""script that takes in an argument and displays all values
+   in the states table of hbtn_0e_0_usa where name matches the argument.
+"""
 
 if __name__ == "__main__":
     import MySQLdb
@@ -11,8 +13,8 @@ if __name__ == "__main__":
                            passwd=argv[2],
                            db=argv[3])
     cursor = mydb.cursor()
-    query = "SELECT * FROM states ORDER BY id ASC"
-    cursor.execute(query)
+    query = "SELECT * FROM states WHERE name='%s' ORDER BY id ASC;"
+    cursor.execute(query, (argv[4],))
     query_rows = cursor.fetchall()
     for row in query_rows:
         print(row)
