7c365b695a01d2f56c6547161ebb16bc6941efc1
Suitret <suitret01@gmail.com>
2023-08-31 15:10:19 -0700

    8. cURL a JSON file

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-# 
+# script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
+curl -s -X POST "$1" -H "Content-Type: application/json" -d @"$2"
