8bdd32cd72c33d2df85647caa94d5a16c280e884
Suitret <suitret01@gmail.com>
2023-08-31 14:49:32 -0700

    5. cURL POST parameters

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl -s -d "email=test@gmail.com&subject='I will always be here for PLD'" -X POST "$1"
+curl -s -d "email=test@gmail.com" -d "subject=I will always be here for PLD" -X POST "$1"
