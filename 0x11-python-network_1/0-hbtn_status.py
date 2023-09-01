8aada9f726b3b2457aa7345c9c353c8fbdde721a
Suitret <suitret01@gmail.com>
2023-09-01 13:20:18 -0700

    0. What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,8 +2,10 @@
 """script that fetches https://alx-intranet.hbtn.io/status
 """
 
+
 if __name__ == "__main__":
     import urllib.request
+
     url = "https://alx-intranet.hbtn.io/status"
     with urllib.request.urlopen(url) as response:
         body_response = response.read()
