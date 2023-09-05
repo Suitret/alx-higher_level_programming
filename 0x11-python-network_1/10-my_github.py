c85cddf51e26c6379cc28913452268e151de7a6e
Suitret <suitret01@gmail.com>
2023-09-05 14:53:41 -0700

    9. My GitHub!

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,15 +1,31 @@
 #!/usr/bin/python3
-"""script that fetches https://alx-intranet.hbtn.io/status
 """
-
+Python script that uses the GitHub API to display your GitHub user ID.
+"""
+import requests
+import sys
 
 if __name__ == "__main__":
-    import urllib.request
+    if len(sys.argv) == 3:
+        username = sys.argv[1]
+        pat = sys.argv[2]
 
-    url = "https://alx-intranet.hbtn.io/status"
-    with urllib.request.urlopen(url) as response:
-        body_response = response.read()
-        print("Body response:")
-        print("\t- type:", type(body_response))
-        print("\t- content:", body_response)
-        print("\t- type:", body_response.decode('utf-8'))
+        # Create a Basic Authentication string using your PAT as the password
+        auth = (username, pat)
+
+        # Define the GitHub API endpoint for user information
+        url = 'https://api.github.com/user'
+
+        try:
+            # Send a GET request to the GitHub API with Basic Authentication
+            response = requests.get(url, auth=auth)
+            data = response.json()
+
+            if 'id' in data:
+                print(data['id'])
+            else:
+                print("None")
+        except requests.exceptions.RequestException as e:
+            print("None")
+    else:
+        print("None")
