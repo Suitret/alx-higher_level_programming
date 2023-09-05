f8959852d7bc9d441e1cf06042da2e0e7f9e6647
Suitret <suitret01@gmail.com>
2023-09-05 15:03:44 -0700

    10. Time for an interview!

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,15 +1,36 @@
 #!/usr/bin/python3
-"""script that fetches https://alx-intranet.hbtn.io/status
 """
-
+script that lists 10 commits (from most recent to oldest) of a repository.
+"""
+import requests
+import sys
 
 if __name__ == "__main__":
-    import urllib.request
+    if len(sys.argv) == 3:
+        repository_name = sys.argv[1]
+        owner_name = sys.argv[2]
 
-    url = "https://alx-intranet.hbtn.io/status"
-    with urllib.request.urlopen(url) as response:
-        body_response = response.read()
-        print("Body response:")
-        print("\t- type:", type(body_response))
-        print("\t- content:", body_response)
-        print("\t- type:", body_response.decode('utf-8'))
+        url = (
+            f"https://api.github.com/repos/{owner_name}/{repository_name}"
+            "/commits"
+        )
+
+        try:
+            # Make a GET request to the GitHub API
+            response = requests.get(url)
+
+            # Check if the request was successful
+            if response.status_code == 200:
+                commits = response.json()
+
+                # Print the most recent 10 commits
+                for commit in commits[:10]:
+                    sha = commit["sha"]
+                    author_name = commit["commit"]["author"]["name"]
+                    print(f"{sha}: {author_name}")
+            else:
+                print("Error: Unable to fetch commits")
+        except requests.exceptions.RequestException as e:
+            print("Error: Unable to connect to GitHub API")
+    else:
+        print("Usage: ./100-github_commits.py <repository_name> <owner_name>")
