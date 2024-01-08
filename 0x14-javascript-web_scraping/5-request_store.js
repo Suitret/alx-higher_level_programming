d8c5bfa175b59320ab45a8b6a4279db51de95e59
Suitret <suitret01@gmail.com>
2024-01-08 02:45:41 -0800

    5. Loripsum

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,19 +1,28 @@
 #!/usr/bin/node
+const request = require('request');
 const fs = require('fs');
 
-const filePath = process.argv[2]; // Get the file path from command line arguments.
+const url = process.argv[2]; // Get the URL from command line arguments.
+const filePath = process.argv[3]; // Get the file path from command line arguments.
 
-// Check if the file path was provided as an argument.
-if (!filePath) {
-  console.error('Usage: node 0-readme.js <file-path>');
+// Check if both URL and file path were provided as arguments.
+if (!url || !filePath) {
+  console.error('Usage: node 5-request_store.js <URL> <file-path>');
   process.exit(1);
 }
 
-// Read the content of the file in utf-8 encoding.
-fs.readFile(filePath, 'utf8', (error, data) => {
+// Make a GET request to the specified URL using the "request" module.
+request.get(url, (error, response, body) => {
   if (error) {
-    console.error(error); // Print the error object if an error occurred.
+    console.error('Error:', error);
+  } else if (response.statusCode === 200) {
+    // Write the response body to the specified file.
+    fs.writeFile(filePath, body, 'utf8', (writeError) => {
+      if (writeError) {
+        console.error('Error writing to file:', writeError);
+      }
+    });
   } else {
-    console.log(data); // Print the file content if reading was successful.
+    console.error('HTTP request failed. Status code:', response.statusCode);
   }
 });
