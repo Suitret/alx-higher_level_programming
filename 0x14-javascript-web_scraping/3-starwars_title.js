79b80f98ff1052abb91f304dbaaa5ff9f4ccee7b
Suitret <suitret01@gmail.com>
2024-01-08 02:42:36 -0800

    3. Star wars movie title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,19 +1,25 @@
 #!/usr/bin/node
-const fs = require('fs');
+const request = require('request');
 
-const filePath = process.argv[2]; // Get the file path from command line arguments.
+const movieId = process.argv[2]; // Get the movie ID from command line arguments.
 
-// Check if the file path was provided as an argument.
-if (!filePath) {
-  console.error('Usage: node 0-readme.js <file-path>');
+// Check if the movie ID was provided as an argument.
+if (!movieId) {
+  console.error('Usage: node 3-starwars_title.js <movie-ID>');
   process.exit(1);
 }
 
-// Read the content of the file in utf-8 encoding.
-fs.readFile(filePath, 'utf8', (error, data) => {
+// Define the URL for the Star Wars API with the provided movie ID.
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+// Make a GET request to the Star Wars API.
+request.get(apiUrl, (error, response, body) => {
   if (error) {
-    console.error(error); // Print the error object if an error occurred.
+    console.error('Error:', error);
+  } else if (response.statusCode === 200) {
+    const movieData = JSON.parse(body);
+    console.log(movieData.title);
   } else {
-    console.log(data); // Print the file content if reading was successful.
+    console.error('Movie not found. Status code:', response.statusCode);
   }
 });
