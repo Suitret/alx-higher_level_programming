c2068be0eb0af39a93df36e8fdaa6bfd3c3582b9
Suitret <suitret01@gmail.com>
2024-01-09 23:15:28 -0800

    3. Add  class

--- 0x15-javascript-web_jquery/3-script.js
+++ 0x15-javascript-web_jquery/3-script.js
@@ -1,1 +1,10 @@
-  document.querySelector('header').style.color = '#FF0000';
+$(document).ready(function() {
+  // Wait for the DOM to be ready
+
+  // Attach a click event handler to the DIV#red_header element
+  $('#red_header').click(function() {
+    // Add the class 'red' to the <header> element
+    $('header').addClass('red');
+  });
+})
+
